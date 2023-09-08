48d590233abd0da4bb05b058f66b72da8b4d3097
daviguima <dvdgmf@gmail.com>
2023-09-08 16:17:06 +0200

    added command line capabilities

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
 import time
 import argparse
-from getpak.commons import Utils as u
+
+import getpak
+from getpak.commons import Utils as U
+from getpak import automation as A
 
 
 """
 Author: David Guimaraes - dvdgmf@gmail.com
 """
 
-
-def main():
-    # ,-------------,
-    # | ENTRY POINT |
-    # '-------------'
-    print('''
-        _..._
-      .'     '.      _
-     /    .-""-\   _/ \ 
-   .-|   /:.   |  |   | 
-   |  \  |:.   /.-'-./ 
-   | .-'-;:__.'    =/  ,ad8888ba,  88888888888 888888888888                          88
-   .'=  *=|CNES _.='  d8"'    `"8b 88               88                               88
-  /   _.  |    ;     d8'           88               88                               88
- ;-.-'|    \   |     88            88aaaaa          88        8b,dPPYba,  ,adPPYYba, 88   ,d8
-/   | \    _\  _\    88      88888 88"""""          88 aaaaaa 88P'    "8a ""     `Y8 88 ,a8"
-\__/'._;.  ==' ==\   Y8,        88 88               88 """""" 88       d8 ,adPPPPP88 8888[
-         \    \   |   Y8a.    .a88 88               88        88b,   ,a8" 88,    ,88 88`"Yba,
-         /    /   /    `"Y88888P"  88888888888      88        88`YbbdP"'  `"8bbdP"Y8 88   `Y8a
-         /-._/-._/                                            88
-         \   `\  \                                            88
-          `-._/._/
-                    ''')
-    
-    pass
-
-
+# ,-------------,
+# | ENTRY POINT |
+# '-------------'
 if __name__ == '__main__':
     # ,--------------,
     # | Start timers |
     # '--------------'
-    u.tic()
+    U.tic()
     t1 = time.perf_counter()
-    # ,-----,
-    # | RUN |
-    # '-----'
-    main()
+
+    # ,------,
+    # | LOGO |
+    # '------'
+    U.print_logo()
+
+    # ,-----------------,
+    # | Present options |
+    # '-----------------'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--version', help='Displays current package version.', action='store_true')
+    parser.add_argument('-mg', help="Given a path, return GRS metadata", default='local', type=str)
+    args = parser.parse_args().__dict__  # Converts the input arguments from Namespace() to dict
+
+    print('User Input Arguments:')
+    for key in args:
+        print(f'{key}: {args[key]}')
+
+    # ,----------------------------------------,
+    # | Automation pipelines class declaration |
+    # '----------------------------------------'
+    gpk_pipe = A.Pipelines()
+
+    # ,---------------,
+    # | Treat options |
+    # '---------------'
+    if args['version']:
+        print(f'GET-Pak version: {getpak.__version__}')
+    elif args['mg']:
+        gpk_pipe.get_grs_metadict(args['mg'])
+    else:
+        print('No commands supplied, exiting.\n')
+
     # ,------------------------------,
     # | End timers and report to log |
     # '------------------------------'
-    t_hour, t_min, t_sec = u.tac()
+    t_hour, t_min, t_sec, _ = U.tac()
     t2 = time.perf_counter()
     final_msg_1 = f'Finished in {round(t2 - t1, 2)} second(s).'
     final_msg_2 = f'Elapsed execution time: {t_hour}h : {t_min}m : {t_sec}s'
